034903e8b8db8e480946d61fb941502d1a412c99
Anthony Underwood <anthony@q0.bioinformatics>
2008-05-27 14:41:01 +0100

    use ask method for HIghline not Highline.ask

--- lib/primehosting/database.rb
+++ lib/primehosting/database.rb
@@ -2,9 +2,11 @@ require 'highline/import'
 
 Capistrano::Configuration.instance(true).load do
   set :database_name, nil
-  set :database_user, Proc.new { Highline.ask("What is your database username?  ") { |q| q.default = "dbuser" } }
-  set :database_pass, Proc.new { Highline.ask("What is your database password?  ") { |q| q.echo = "*" } }
+  set :database_user, Proc.new { ask("What is your database username?  ") { |q| q.default = "dbuser" } }
+  set :database_pass, Proc.new { ask("What is your database password?  ") { |q| q.echo = "*" } }
   
+  puts database_user
+  puts database_pass
   namespace :database do
     task :configure do
       db_config =<<EOF
